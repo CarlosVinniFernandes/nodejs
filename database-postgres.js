233b97f82a191c0f77916c6e8b2b5e4f3b60c9fe
carlosvinnifernandes <carlos24070061@aluno.cesupa.br>
2025-03-12 08:57:58 -0300

    Api Node com Postgres

--- database-postgres.js
+++ database-postgres.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { randomUUID } from 'node:crypto'
 import { sql } from './db.js'
 
@@ -35,4 +36,43 @@ export class DataBasePostgres {
         await sql`delete from videos where id = ${id}`
 
     }
+=======
+import { randomUUID } from 'node:crypto'
+import { sql } from './db.js'
+
+export class DataBasePostgres {
+    async list(search) {
+
+        let videos 
+        if(search){
+            // operações assicronas -> elas acontecem, mas levam um tempo
+            videos = sql `select * from videos where title ilike ${'%' + search + '%'}`
+        } else {
+            videos = sql `select * from videos`
+        }
+
+        return videos
+    }
+
+    async create(video) {
+        const videoID = randomUUID()
+        const { title, description, duration } = video
+
+        await sql`insert into videos (id, title, description, duration) VALUES (${videoID}, ${title}, ${description}, ${duration})`
+    }
+
+    async update(id, video) {
+        const {title, description, duration} = video
+
+        await sql`update videos set title = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id}`
+
+
+    }
+
+    async delete (id) {
+
+        await sql`delete from videos where id = ${id}`
+
+    }
+>>>>>>> e37509b (Api Node com Postgres)
 }
