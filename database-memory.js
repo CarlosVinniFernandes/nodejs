e90824b20de2660e176317f21ccd4ab5d0719b9f
carlosvinnifernandes <carlos24070061@aluno.cesupa.br>
2025-03-12 10:12:42 -0300

    API Node com Postgres

--- database-memory.js
+++ database-memory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { randomUUID } from 'node:crypto'
 
 export class DataBaseMemory {
@@ -38,45 +37,4 @@ export class DataBaseMemory {
     delete(id) {
         this.#videos.delete(id)
     }
-=======
-import { randomUUID } from 'node:crypto'
-
-export class DataBaseMemory {
-    #videos = new Map()
-
-    // Set -> Quase array, mas não aceita valores duplicados
-
-    list(search) {
-        return Array.from(this.#videos.entries()).map((videoArray) =>{
-            const id = videoArray[0]
-            const data = videoArray[1]
-
-            return{
-                id,
-                ...data
-            }
-        })
-        .filter(video =>{
-            if(search){
-                return video.title.includes(search)
-            }
-            return true
-        })
-    
-    }
-
-    create(video) {
-        const videoID = randomUUID() // Retorna um ID unico
-
-        this.#videos.set(videoID, video)
-    }
-
-    update(id, video) {
-        this.#videos.set(id, video)
-    }
-
-    delete(id) {
-        this.#videos.delete(id)
-    }
->>>>>>> e37509b (Api Node com Postgres)
 }
--- db.js
+++ db.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import 'dotenv/config';
 import postgres from 'postgres';
 import { neon } from '@neondatabase/serverless';
 
-=======
-import 'dotenv/config';
-import postgres from 'postgres';
-import { neon } from '@neondatabase/serverless';
-
->>>>>>> e37509b (Api Node com Postgres)
 export const sql = neon(process.env.DATABASE_URL);
